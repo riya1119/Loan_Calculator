fc17ae6d5113a6eef90b9e2dfe443e1fae3139e0
Riya Jain <76917573+riya1119@users.noreply.github.com>
2021-05-17 12:49:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './components/calculator/Calculator';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <Calculator />
+     
+    </div>
+  );
+}
+
+export default App;
